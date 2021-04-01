afc40bcf3182c462085c9ecf5ef65ab7150f665e
kansuki20 <74278396+kansuki20@users.noreply.github.com>
2021-04-01 19:54:45 +0900

    2021/04/01 자바 자료구조 - Linked Stack

---
+++ 0401 Linked Stack 01/Stack.java
@@ -0,0 +1,9 @@
+package week05_SL_Stack_01;
+
+public interface Stack {
+	boolean isEmpty();
+	void push(char item);
+	char pop();
+	void delete();
+	char peek();
+}
---
+++ 0401 Linked Stack 01/StackNode.java
@@ -0,0 +1,6 @@
+package week05_SL_Stack_01;
+
+class StackNode {
+	char data;
+	StackNode link;
+}
