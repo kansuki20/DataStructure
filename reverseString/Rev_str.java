456130e881699a2107961ea5e22b6aac4a1dae5b
kansuki20 <74278396+kansuki20@users.noreply.github.com>
2021-04-15 15:51:11 +0900

    2021/04/15 자바 자료구조 - ReverseString

---
+++ reverseString/Rev_str.java
@@ -0,0 +1,19 @@
+package week06_reverseString;
+
+import java.util.Stack;
+
+public class Rev_str {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Character> stack = new Stack<Character>();
+		String str = "거꾸로";
+		
+		for(int i=0; i<str.length(); i++) 
+			stack.push(str.charAt(i));
+		
+		while(!stack.empty())
+			System.out.print(stack.pop());
+	}
+
+}
