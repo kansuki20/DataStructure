71037cb78f6997e58fe9cfdab6fe3cdb5cf9f5a7
kansuki20 <74278396+kansuki20@users.noreply.github.com>
2021-04-16 22:08:11 +0900

    2021/04/16 자바 자료구조 - ArrayQueue

---
+++ 0416 ArrayQueue/Queue_Test.java
@@ -0,0 +1,33 @@
+package week07_ArrayQueue;
+
+public class Queue_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int queueSize = 3;
+		char deletedItem;
+		ArrayQueue Q = new ArrayQueue(queueSize);
+		
+		Q.enQueue('A');
+		Q.printQueue();
+		
+		Q.enQueue('B');
+		Q.printQueue();
+		
+		deletedItem = Q.deQueue();
+		if(deletedItem != 0)
+			System.out.println("deleted Item : " + deletedItem);
+		Q.printQueue();
+		
+		deletedItem = Q.deQueue();
+		if(deletedItem != 0)
+			System.out.println("deleted Item : " + deletedItem);
+		Q.printQueue();
+		
+		Q.enQueue('c');
+		Q.printQueue();
+		
+		
+	}
+
+}
